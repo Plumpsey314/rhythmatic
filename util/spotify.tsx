697ec1bbe8a75ae2d22847d92bac0a8a9e75e3f9
Cooper Saye <coopersaye@gmail.com>
2023-01-31 23:52:24 -0500

    Implement access token retrieval.

--- util/spotify.tsx
+++ util/spotify.tsx
@@ -4,3 +4,19 @@ const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 const TRACKS_ENDPOINT = 'https://api.spotify.com/v1/me/tracks';
 const SEARCH_ENDPOINT = 'https://api.spotify.com/v1/search';
+
+async function getAccessToken(refresh_token: string) {
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token,
+    }),
+  });
+
+  return response.json();
+};
