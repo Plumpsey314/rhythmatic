d5264ac2b2d9c4ad3f47d0f0c8f30deffce5ad62
Cooper Saye <coopersaye@gmail.com>
2023-02-01 11:06:18 -0500

    Construct index page content.

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,72 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      <Header session={session} />
+      <div className={styles.content}>
+        {
+          loading &&
+          <div className={styles.loading}>
+            <p>{loading}</p>
+            <LinearProgress />
+          </div>
+        }
+        {
+          tracks &&
+          <div className={styles.reset}>
+            <button onClick={() => {
+              setTracks(undefined);
+            }}>
+              Want to try again? Reset
+            </button>
+          </div>
+        }
+        {
+          (!loading && !tracks) &&
+          <div className={styles.form}>
+            <h1>
+              <Image
+                className={styles.logo}
+                src="/img/logo.png"
+                width="48"
+                height="48"
+                alt="logo.png"
+              />
+              Finetune
+            </h1>
+            <p>Finetune&trade; combines Spotify and ChatGPT to give you the very best music recommendations! Try it below:</p>
+            <form onSubmit={e => {
+              e.preventDefault();
+              generateSongs();
+            }}>
+              <ThemeProvider theme={theme}>
+                <TextField
+                  type="text"
+                  placeholder="Describe your ideal music..."
+                  value={text}
+                  onChange={e => setText(e.target.value)}
+                  multiline
+                  sx={{ width: '400px' }}
+                  required
+                />
+              </ThemeProvider>
+              <button>Find music for me!</button>
+            </form>
+          </div>
+        }
+        {
+          (!loading && tracks) &&
+          <div className={styles.tracks}>
+            {
+              tracks.map((track, i) =>
+                <Track session={session} {...track} key={i} />
+              )
+            }
+          </div>
+        }
+      </div>
+      <p className={styles.copyright}>
+        &copy; {new Date().getFullYear()} Prodigy Development
+      </p>
     </div>
   );
 }
