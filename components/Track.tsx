c0b8c783e8a30ae19cc82c90f9deb83e2f5c0467
Cooper Saye <coopersaye@gmail.com>
2023-02-01 18:23:55 -0500

    Update track layout.

--- components/Track.tsx
+++ components/Track.tsx
@@ -5,7 +5,10 @@ import { useState } from 'react';
 import styles from '../styles/components/Track.module.scss';
 
 export default function Track(props: any) {
-  const { name, id, artists, album, external_urls, session } = props;
+  const {
+    name, id, artists, album, external_urls, genres,
+    preview_url, popularity, duration_ms, session, explicit
+  } = props;
 
   const [saved, setSaved] = useState(false);
 
@@ -14,6 +17,8 @@ export default function Track(props: any) {
     setSaved(true);
     await fetch(`/api/savetrack?id=${encodeURIComponent(id)}`);
   };
+
+  if (!external_urls || !artists || !album) return null;
 
   return (
     <div className={styles.container}>
@@ -27,6 +32,14 @@ export default function Track(props: any) {
           <p>{artists.map((artist: any) => artist.name).join(', ')}</p>
           <p>{album.name}</p>
         </div>
+        <audio src={preview_url} controls />
+        <p>
+          Popularity: {popularity}
+          <br />Release date: {album.release_date}
+          <br />Length: {duration_ms / 1000} sec
+          <br />Explicit: {explicit ? 'Yes' : 'No'}
+          <br />Spotify ID: {id}
+        </p>
         <span className={styles.flexfill} />
       </a>
       <div className={styles.save}>
