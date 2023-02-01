62b46ce5188ea18d5f660dcc8e6de896bb71affb
Cooper Saye <coopersaye@gmail.com>
2023-01-31 23:50:22 -0500

    Link outfit font.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
