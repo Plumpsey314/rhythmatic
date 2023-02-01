8de363ab88b6f8f4ddf48e2ed8e8b4599b207843
Cooper Saye <coopersaye@gmail.com>
2023-02-01 11:06:09 -0500

    Update index page props.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,24 @@
+import Header from '@/components/Header';
+import Track from '@/components/Track';
 import styles from '@/styles/pages/Index.module.scss';
+import { createTheme, LinearProgress, TextField, ThemeProvider } from '@mui/material';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  const [text, setText] = useState("");
+  const [tracks, setTracks] = useState<any[]>();
+  const [loading, setLoading] = useState<string | null>(null);
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Outfit, sans-serif'
+    }
+  });
+
   return (
     <div className={styles.container}>
     </div>
