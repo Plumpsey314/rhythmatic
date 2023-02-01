7c570e3786937174508428c15eb6e9d85ba3dcfd
Cooper Saye <coopersaye@gmail.com>
2023-01-31 23:53:33 -0500

    Update header props.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,14 @@
+import { signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
 import styles from '../styles/components/Header.module.scss';
 
-export default function Header() {
+type Props = {
+  session: any;
+};
+
+export default function Header(props: Props) {
+  const { session } = props;
+
   return (
     <div className={styles.container}>
     </div>
