394ba275b6fd749c456512f3becc158a102e0781
Cooper Saye <coopersaye@gmail.com>
2023-02-01 11:04:09 -0500

    Update track props.

--- components/Track.tsx
+++ components/Track.tsx
@@ -1,6 +1,20 @@
+import AddIcon from '@mui/icons-material/Add';
+import CheckIcon from '@mui/icons-material/Check';
+import { Tooltip } from '@mui/material';
+import { useState } from 'react';
 import styles from '../styles/components/Track.module.scss';
 
-export default function Track() {
+export default function Track(props: any) {
+  const { name, id, artists, album, external_urls, session } = props;
+
+  const [saved, setSaved] = useState(false);
+
+  // saves given track to library
+  async function saveTrack(id: string) {
+    setSaved(true);
+    await fetch(`/api/savetrack?id=${encodeURIComponent(id)}`);
+  };
+
   return (
     <div className={styles.container}>
     </div>
