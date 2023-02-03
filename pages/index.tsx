529f9883198bd0378eac33ea8aedfd393a9ce6d0
Cooper Saye <coopersaye@gmail.com>
2023-02-03 12:07:32 -0500

    Rework track search process.

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,28 +34,6 @@ export default function Home() {
     }
   });
 
-  // gets data for given tracks
-  async function getTracks(tracksData: string[]) {
-    setLoading(null);
-    setTracks([]);
-    let index = 0;
-    let allTracks: any[] = [];
-    async function makeRequest(tracksData: string[]) {
-      if (index === tracksData.length) {
-        setTracks(allTracks);
-        return;
-      }
-      const trackData = tracksData[index];
-      const [song, artist] = trackData.split('\n');
-      const response = await fetch(`/api/searchtrack?song=${encodeURIComponent(song)}&artist=${encodeURIComponent(artist)}}`);
-      const data = await response.json();
-      const track = data?.tracks?.items[0];
-      if (track) {
-        setTracks(tracks => tracks ? [...tracks, track] : [track]);
-        allTracks.push(track);
-      }
-      index++;
-      makeRequest(tracksData);
   // load local storage on start
   useEffect(() => {
     const newPrompt = window.localStorage.getItem('finetune-prompt');
@@ -68,8 +46,25 @@ export default function Home() {
     if (newTemperature !== null) setTemperature(parseFloat(newTemperature));
   }, []);
 
+  // gets tracks from spotify with given url
+  async function getTracks(url: string) {
+    const response = await fetch(`/api/searchtrack?url=${encodeURIComponent(url)}`);
+    // handle api error
+    if (response.status !== 200) {
+      window.alert(`Something went wrong searching tracks: ${response.status} ${response.statusText}`);
+      setLoading(null);
+      return;
     }
-    makeRequest(tracksData);
+    const data = await response.json();
+    const items = data?.tracks?.items;
+    console.log(items);
+    if (!items?.length) {
+      window.alert('Spotify returned no tracks.');
+      setLoading(null);
+      return;
+    }
+    setTracks(items);
+    setLoading(null);
   }
 
   // generates songs from chatgpt
@@ -85,7 +80,10 @@ export default function Home() {
     setLoading('Sending your preferences to ChatGPT...')
 
     // return if no text
-    if (!text || !text.trim()) return;
+    if (!text || !text.trim()) {
+      window.alert('Please enter some text.');
+      return;
+    }
 
     // make request to chatgpt
     const response = await fetch("/api/openai", {
@@ -93,7 +91,7 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ text })
+      body: JSON.stringify({ text, model, temperature, maxTokens, prompt })
     });
 
     // clear text
@@ -106,31 +104,16 @@ export default function Home() {
       throw data.error || new Error(`Request failed with status ${response.status}`);
     }
 
-    console.log(data.result);
-
-    // parse raw result
-    let raw = data.result;
-    const bracketIndex = raw.indexOf('[');
-    if (bracketIndex === -1) {
+    // check result
+    const result = data.result.trim();
+    console.log(result);
+    if (!result.startsWith('https://api.spotify.com/v1/search?')) {
       setLoading(null);
-      window.alert(`Invalid result from ChatGPT:\n${raw ? raw : 'No response'}`);
-      throw 'invalid result';
-    }
-    raw = raw.substring(bracketIndex);
-    console.log(raw);
-
-    // parse song array
-    let songArray: string[];
-    try {
-      songArray = JSON.parse(raw);
-    } catch (e) {
-      setLoading(null);
-      throw `Something went wrong parsing the result: ${e}`
+      window.alert(`Invalid response from ChatGPT:\n${result ? result : 'No response'}`);
     }
 
-    // get tracks from song data
-    console.log(songArray);
-    getTracks(songArray);
+    // search tracks with result url
+    getTracks(result);
   }
 
   return (
@@ -168,6 +151,7 @@ export default function Home() {
               Finetune
             </h1>
             <p>Finetune&trade; combines Spotify and ChatGPT to give you the very best music recommendations! Try it below:</p>
+            <Button onClick={() => setSettingsOpen(true)}>OpenAI Settings</Button>
             <form onSubmit={e => {
               e.preventDefault();
               generateSongs();
@@ -271,7 +255,7 @@ export default function Home() {
           </DialogContent>
         </Dialog>
         {
-          (!loading && tracks) &&
+          tracks &&
           <div className={styles.tracks}>
             {
               tracks.map((track, i) =>
