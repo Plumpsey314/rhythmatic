1ec937ed748af30a10a1a5f95eee5699faa7f233
Cooper Saye <coopersaye@gmail.com>
2023-02-04 03:10:52 -0500

    Minor fixes.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600&display=swap" rel="stylesheet" />
+        <link href="https://api.fontshare.com/v2/css?f[]=clash-grotesk@400,600&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- util/prompt.tsx
+++ util/prompt.tsx
@@ -4,7 +4,7 @@ export function getPrompt() {
   p += "https://api.spotify.com/v1/search?q=<OUTPUT>&type=track&limit=10\n"
   p += "Here are the valid tags: genre, artist, year\n";
   p += "Here are all the valid genres: acoustic, afrobeat, alt-rock, alternative, ambient, anime, black-metal, bluegrass, blues, bossanova, brazil, breakbeat, british, cantopop, chicago-house, children, chill, classical, club, comedy, country, dance, dancehall, death-metal, deep-house, detroit-techno, disco, disney, drum-and-bass, dub, dubstep, edm, electro, electronic, emo, folk, forro, french, funk, garage, german, gospel, goth, grindcore, groove, grunge, guitar, happy, hard-rock, hardcore, hardstyle, heavy-metal, hip-hop, holidays, honky-tonk, house, idm, indian, indie, indie-pop, industrial, iranian, j-dance, j-idol, j-pop, j-rock, jazz, k-pop, kids, latin, latino, malay, mandopop, metal, metal-misc, metalcore, minimal-techno, movies, mpb, new-age, new-release, opera, pagode, party, philippines-opm, piano, pop, pop-film, post-dubstep, power-pop, progressive-house, psych-rock, punk, punk-rock, r-n-b, rainy-day, reggae, reggaeton, road-trip, rock, rock-n-roll, rockabilly, romance, sad, salsa, samba, sertanejo, show-tunes, singer-songwriter, ska, sleep, songwriter, soul, soundtracks, spanish, study, summer, swedish, synth-pop, tango, techno, trance, trip-hop, turkish, work-out, world-music\n";
-  p += "ONLY USE VALID GENRES AND TAGS.\n\n";
+  p += "ONLY USE VALID GENRES AND TAGS. MAKE RESPONSES AS SIMPLE AS POSSIBLE.\n\n";
   p += "songs by pink floyd -> artist:Pink+Floyd\n";
   p += 'certified lover boy music -> "Certified Lover Boy"+artist:Drake\n';
   p += "electronic music fun -> fun+genre:electronic\n";
