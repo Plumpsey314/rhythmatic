8d7de61709922f6f9bee1b36501962b0443093c9
Cooper Saye <coopersaye@gmail.com>
2023-02-07 00:59:38 -0500

    Rename project.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Finetune</title>
+        <title>Rhythmatic</title>
         <link rel="icon" href="/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
