a1d75d1fee22e573de1440382c4f3a51dbf4dc25
Cooper Saye <coopersaye@gmail.com>
2023-02-09 23:50:49 -0500

    Define firebase config.

---
+++ util/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyACJUHxQNVybyLJllexlBauTfVwfJ_jrKc",
+  authDomain: "prodigy-development.firebaseapp.com",
+  projectId: "prodigy-development",
+  storageBucket: "prodigy-development.appspot.com",
+  messagingSenderId: "237879702030",
+  appId: "1:237879702030:web:bdeef1541d55503babc25f"
+};
