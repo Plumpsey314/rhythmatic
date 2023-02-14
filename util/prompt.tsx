a5b37a98a517a17aa4a74403c9f44e42fed0f271
swankyboss <l8onschur@gmail.com>
2023-02-14 22:48:19 +0100

    betterprompt

--- util/prompt.tsx
+++ util/prompt.tsx
@@ -1,12 +1,13 @@
 export function getPrompt() {
-  let prompt = ` Build a fast AI music chatbot. Recommend ten unique songs matching user query. Prioritize relevance, diversity, serendipity, and popularity/recency. Handle 'similar' queries, excluding specified artists. Return recommendations as JS arrays [song, artist].`
-  prompt += `Example: Query = 'find me happy pop songs' output = '["Happy\\nPharrell Williams", "Can't Stop the Feeling!\\nJustin Timberlake", "Shut Up and Dance\\nWalk the Moon", etc]."'\n\n`
+  let prompt = `Create AI music chatbot. Suggest ten unique songs matching user query. Prioritize relevance, diversity, serendipity, and popularity/recency. Handle 'similar' queries, exclude specified artists. Return recommendations as JS arrays [song, artist].`
+  
+  prompt += `Example: Query 'happy pop songs' -> ["Happy\\nPharrell Williams", "Can't Stop the Feeling!\\nJustin Timberlake", "Shut Up and Dance\\nWalk the Moon", etc].`
   return prompt;
 }
  
  
 
 export function getReprompt(tracks: string) {
-  let prompt = `Refine the song list: ${tracks}. Get updated 10 recommendations in the same format.\n\n`;
+  let prompt = `Refine the song list using same prompt: ${tracks}. Get updated 10 recommendations in the same format.\n\n`;
   return prompt;
 }
