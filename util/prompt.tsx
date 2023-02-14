a0e0161c2f1389d585529174c9c6725797fa4534
swankyboss <l8onschur@gmail.com>
2023-02-14 20:03:08 +0100

    fixing one character error

--- util/prompt.tsx
+++ util/prompt.tsx
@@ -1,5 +1,5 @@
 export function getPrompt() {
-  let prompt = ` Build a fast AI music chatbot. Recommend teb unique songs matching user query. Prioritize relevance, diversity, serendipity, and popularity/recency. Handle 'similar' queries, excluding specified artists. Return recommendations as JS arrays [song, artist].`
+  let prompt = ` Build a fast AI music chatbot. Recommend ten unique songs matching user query. Prioritize relevance, diversity, serendipity, and popularity/recency. Handle 'similar' queries, excluding specified artists. Return recommendations as JS arrays [song, artist].`
   prompt += `Example: Query = 'find me happy pop songs' output = '["Happy\\nPharrell Williams", "Can't Stop the Feeling!\\nJustin Timberlake", "Shut Up and Dance\\nWalk the Moon", etc]."'\n\n`
   return prompt;
 }
