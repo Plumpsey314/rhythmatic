d00243d76c9f740a89c96488d611f07916d7dc32
swankyboss <l8onschur@gmail.com>
2023-02-15 03:01:52 +0100

    metatagupdate?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,7 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>Rhythmatic</title>
+        <meta name="description" content="Discover new music with Rhythmatic.ai, the music recommendation service powered by OpenAI's APIs and connected to Spotify. Get personalized recommendations tailored to your tastes and discover your new favorite artists."/>
         <link rel="icon" href="/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
