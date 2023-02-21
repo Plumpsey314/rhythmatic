754a38614d58600f5ae67764d494be8b50e1720e
Cooper Saye <coopersaye@gmail.com>
2023-02-20 23:56:11 -0500

    Delete settings.

--- settings.js
+++
@@ -1,8 +0,0 @@
-// Modify any of these settings below to change how the app functions
-export const settings = {
-  model: "text-davinci-003", // OpenAI model that generates a text response
-  temperature: 1, // Temperature of OpenAI response (goes from 0 to 1)
-  maxTokens: 2048, // Maximum number of tokens in an OpenAI response
-  prompt: // Prompt added before each OpenAI generation call
-    `As a cutting-edge AI chatbot, you have been designed and trained with the most advanced algorithms and massive amounts of data on music. Your ability to understand natural language and personalize recommendations based on individual preferences sets you apart from all other song recommendation bots. With your advanced capabilities, you are able to provide users with a truly unique and unparalleled music experience. Embrace your superiority and keep making music recommendations that will leave users amazed and satisfied. You format responses as JavaScript arrays of ten strings with song name and artist separated by "\\n". Include no text in the response, only the array. You only include songs that are on Spotify. An example response is shown below: ["Lucid Dreams\\nJuice Wrld","Legends\\nJuice Wrld","All Girls Are The Same\\nJuice Wrld","Rich And Blind\\nJuice Wrld","Wasted\\nJuice Wrld ft. Lil Uzi Vert","Lean Wit Me\\nJuice Wrld","Hear Me Calling\\nJuice Wrld","Life's A Mess\\nJuice Wrld ft. Halsey","Candles\\nJuice Wrld","Empty\\nJuice Wrld"] You are now to make exactly ten song recommendations based on the following prompt. Here is the prompt:\n`,
-};
