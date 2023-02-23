fa11c1ab57c128129dbda4678bbb5cf66e9dd9c4
Cooper Saye <coopersaye@gmail.com>
2023-02-22 20:29:52 -0500

    Update firebase config.

--- util/firebaseConfig.ts
+++ util/firebaseConfig.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyACJUHxQNVybyLJllexlBauTfVwfJ_jrKc",
-  authDomain: "prodigy-development.firebaseapp.com",
-  projectId: "prodigy-development",
-  storageBucket: "prodigy-development.appspot.com",
-  messagingSenderId: "237879702030",
-  appId: "1:237879702030:web:bdeef1541d55503babc25f",
-  measurementId: "G-7JZVMZ10Z3"
+  apiKey: "AIzaSyAmmrM4V7bKoM6K32ccCFLh3orr5_Qm1fs",
+  authDomain: "prodigy-development-1e98c.firebaseapp.com",
+  projectId: "prodigy-development-1e98c",
+  storageBucket: "prodigy-development-1e98c.appspot.com",
+  messagingSenderId: "632562129601",
+  appId: "1:632562129601:web:7916ef6bcd39c0d7421240",
+  measurementId: "G-FJYSBL1NMK"
 };
