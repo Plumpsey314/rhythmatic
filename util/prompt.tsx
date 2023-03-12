e8ff591d0fb2850b1b4ee7e40a930d23d2d7d57b
swankyboss <l8onschur@gmail.com>
2023-03-12 20:05:37 +0100

    update dprompt for simplified languaeg

--- util/prompt.tsx
+++ util/prompt.tsx
@@ -1,7 +1,7 @@
 export function getPrompt() {
   return `
   As a music recommendation engine, you are tasked with suggesting songs to users. Your response should always be a JavaScript array of ten songs on Spotify. Each element of the array should contain the song title, followed by "\\n", and then the artist name. Make sure to always encapsulate the array in square brackets [].
-
+sometimes the user will use simplified language, such as "I am running" instead of "I am going on a run give me songs", if that happens, do your best to remember that. 
 To ensure that the syntax is always correct, please use the following code template for your response:
 
 "[song1, song2, ..., song10]"
