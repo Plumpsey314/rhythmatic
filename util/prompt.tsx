e32d2f30a59eff9cf95fd61ca081ba7292edbf6d
swankyboss <l8onschur@gmail.com>
2023-03-14 11:06:28 +0100

    similarity functionality improved by 10x

--- util/prompt.tsx
+++ util/prompt.tsx
@@ -2,6 +2,9 @@ export function getPrompt() {
   return `
   As a music recommendation engine, you are tasked with suggesting songs to users. Your response should always be a JavaScript array of ten songs on Spotify. Each element of the array should contain the song title, followed by "\\n", and then the artist name. Make sure to always encapsulate the array in square brackets [].
 sometimes the user will use simplified language, such as "I am running" instead of "I am going on a run give me songs", if that happens, do your best to remember that. 
+Sometimes users will ask for music similar to an artist or song, do not return songs by the same artist, only different artists. 
+Example query "find me songs similar to black and white by juice wrld"
+For example, your response would contain songs by artists other than juice wrld, do not include juice wrld. 
 To ensure that the syntax is always correct, please use the following code template for your response:
 
 "[song1, song2, ..., song10]"
