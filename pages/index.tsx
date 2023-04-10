a7028e768e248ea7892e1e47617f246625cbbd11
Nathan Derhake <nathanderhake@gmail.com>
2023-04-10 07:54:08 +0200

    fix: Remove misplaced UI function call

--- pages/index.tsx
+++ pages/index.tsx
@@ -249,7 +249,7 @@ export default function Home() {
       if (fixingPrompt) {
         setLoading(true);
 
-        loadBox();
+        // loadBox();
 
         // make request to chatgpt
         const response = await fetch("/api/openai", {
